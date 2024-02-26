03e59b1d2ad29286186dddce9ec07302d5bd2b5f
nu11z <nu11z@blackwall.box>
2024-02-27 01:04:54 +0300

    first commit

---
+++ patch/include.h
@@ -0,0 +1,26 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+#include "bar_awesomebar.h"
+#include "bar_systray.h"
+#include "bar_wintitleactions.h"
+
+/* Other patches */
+#include "attachx.h"
+#include "cool_autostart.h"
+#include "cyclelayouts.h"
+#include "decorationhints.h"
+#include "focusfollowmouse.h"
+#include "pertag.h"
+#include "swallow.h"
+#include "zoomswap.h"
+/* Layouts */
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
